7b285760c146a766a0d059f09d37ee4a2895409d
Shashwat Rawat <shashwatrrawat@gmail.com>
2026-02-07 02:32:45 +0530

    style: fix layout of input field

--- src/Components/MainSection/MainUI.jsx
+++ src/Components/MainSection/MainUI.jsx
@@ -1,23 +1,42 @@
+import {useState} from 'react'
+
 export default function MainUI(){
   
-  function valueOutput(value){
-    console.log(value)
+  function handleChange(e) {
+    setValue(Number(e.target.value));
   }
+
+  const [value, setValue] = useState(50)
   
   
   return(
     <section className="main-ui">
       <div className="user-input-section">
         <h2>CSS box-shadow</h2>
-        <label>
-          Horizontal
-          <input 
-            type="range" 
+        <div className="control">
+          <div className="top-row">
+            <label htmlFor="horizontal">Horizontal</label>
+
+            <input
+              id="horizontal"
+              className="num-input"
+              type="number"
+              min="0"
+              max="20"
+              value={value}
+              onChange={handleChange}
+            />
+          </div>
+
+          <input
+            className="range-input"
+            type="range"
             min="0"
-            max="100"
-            onChange={(e)=>valueOutput(e.target.value)}
+            max="20"
+            value={value}
+            onChange={handleChange}
           />
-        </label>
+        </div>
       </div>
       <div className="output-section">
         output
