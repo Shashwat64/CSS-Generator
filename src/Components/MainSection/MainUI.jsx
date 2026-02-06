8a3c84dacb7bc3149175607802a5094310b3e455
Shashwat Rawat <shashwatrrawat@gmail.com>
2026-02-07 01:47:53 +0530

    feat: add functional slider

--- src/Components/MainSection/MainUI.jsx
+++ src/Components/MainSection/MainUI.jsx
@@ -1,8 +1,23 @@
 export default function MainUI(){
+  
+  function valueOutput(value){
+    console.log(value)
+  }
+  
+  
   return(
     <section className="main-ui">
       <div className="user-input-section">
-        input
+        <h2>CSS box-shadow</h2>
+        <label>
+          Horizontal
+          <input 
+            type="range" 
+            min="0"
+            max="100"
+            onChange={(e)=>valueOutput(e.target.value)}
+          />
+        </label>
       </div>
       <div className="output-section">
         output
--- src/Components/MainSection/Navbar.jsx
+++ src/Components/MainSection/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar(){
   return(
     <nav className="navbar">
-      <button>Trial</button>
+      <button className="active">Box Shadow</button>
     </nav>
   )
 }
