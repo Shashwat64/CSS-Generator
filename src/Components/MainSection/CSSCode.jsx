0959cae55c56edd0c1fe86a8f430e88dd890e2c4
Shashwat Rawat <shashwatrrawat@gmail.com>
2026-02-07 19:35:57 +0530

    refactor: correct align title of css code

--- src/Components/MainSection/CSSCode.jsx
+++ src/Components/MainSection/CSSCode.jsx
@@ -2,7 +2,7 @@ export default function CSSCode(props){
   return(
     <section className="css-code-section">
       <div className="copy-btn-div">
-        CSS Code: 
+        <p>CSS Code: </p>
         <button>Copy</button>
       </div>
       <div className="css-code">
