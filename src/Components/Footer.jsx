2a091d5570077373699c72b291b79fe332a51fac
Shashwat Rawat <shashwatrrawat@gmail.com>
2026-02-15 01:42:18 +0530

    fix: update text in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,12 @@
 export default function Footer(){
   return(
     <footer>
-
+      <p className="signture">
+        Built by Shashwat Rawat
+      </p>
       <p>
-        This is footer 
+        View Source Code on 
+        <a href="https://github.com/Shashwat64/CSS-Generator" target="_blank">Github</a>
       </p>
     </footer>
   )
